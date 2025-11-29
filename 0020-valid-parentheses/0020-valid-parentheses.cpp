a3e7acd8e475f74524df5e563dfdbf4cd1a478b7
Shiva Slathia <shivaslathia488@gmail.com>
2025-11-29 17:11:39 +0530

    Time: 0 ms (100%), Space: 8.9 MB (37.83%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        if (s.length() % 2 != 0)
+            return false;
+
+        stack<char> st;
+
+        for (int i = 0; i < s.length(); i++) {
+
+            if (s[i] == '(' || s[i] == '[' || s[i] == '{')
+                st.push(s[i]);
+
+            else {
+                if (st.size() == 0)
+                    return false;
+
+                // check first
+                if (s[i] == ')' && st.top() != '(')
+                    return false;
+                if (s[i] == ']' && st.top() != '[')
+                    return false;
+                if (s[i] == '}' && st.top() != '{')
+                    return false;
+
+                st.pop(); // pop after checking
+            }
+        }
+
+        if (st.size() == 0)
+            return true;
+        else
+            return false;
+    }
+};
