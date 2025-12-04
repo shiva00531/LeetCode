2a211beefe9a8ce9eb496734d7deac9fc01c2eb0
Shiva Slathia <shivaslathia488@gmail.com>
2025-12-04 23:02:12 +0530

    Time: 0 ms (100%), Space: 20.2 MB (43.61%) - LeetHub

---
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        ListNode dummy(0);
+        dummy.next = head;
+        ListNode* cur = &dummy;
+
+        while (cur->next) {
+            if (cur->next->val == val)
+                cur->next = cur->next->next;
+            else
+                cur = cur->next;
+        }
+        return dummy.next;
+    }
+};
